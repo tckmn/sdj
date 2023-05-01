355b50ce9c9488255f3c83259bb7b09cb9540dea
Andy Tockman <andy@tck.mn>
2023-05-01 03:00:07 -0400

    we now have flowers and mountains
    wow

--- live.js
+++ live.js
@@ -1,3 +1,5 @@
+const ctx = new AudioContext();
+
 const eps = 0.1;
 const cw = 800, ch = 100, cs = 50;
 const colors = {
@@ -6,8 +8,78 @@ const colors = {
     cutActive: '#f0f',
     bar:       '#f00'
 };
+const widths = {
+    bar: 2,
+    cut: 3
+};
 const tickRate = 100;
 const lookahead = 0.5;
+
+const trackData = {
+
+    flowerdance: {
+        intro: {
+            next: { name: 'main' }
+        },
+        main: {
+            next: { name: 'main' },
+            cuts: [
+                {t: 15.237, dest: { name: 'outro' }},
+                {t: 30.473, dest: { name: 'outro' }},
+                {t: 45.706, dest: { name: 'outro' }},
+                {t: 60.951, dest: { name: 'outro' }},
+                {t: 91.427, dest: { name: 'outro' }},
+                {t: 106.661, dest: { name: 'outro' }},
+                {t: 121.905, dest: { name: 'outro' }},
+                {t: 139.042, dest: { name: 'outro' }},
+                {t: 0, dest: { name: 'outro' }}
+            ]
+        },
+        outro: {
+            next: undefined
+        }
+    },
+
+    summit: {
+        intro: {
+            next: { name: 'intro', offset: 38.441 },
+            fades: ['intro', 'city', 'oldsite', 'resort', 'cliffside', 'finale']
+        },
+        city: {
+            next: { name: 'city', offset: 38.441 },
+            fades: ['intro', 'city', 'oldsite', 'resort', 'cliffside', 'finale']
+        },
+        oldsite: {
+            next: { name: 'oldsite', offset: 38.441 },
+            fades: ['intro', 'city', 'oldsite', 'resort', 'cliffside', 'finale']
+        },
+        resort: {
+            next: { name: 'resort', offset: 38.441 },
+            fades: ['intro', 'city', 'oldsite', 'resort', 'cliffside', 'finale']
+        },
+        cliffside: {
+            next: { name: 'cliffside', offset: 38.441 },
+            fades: ['intro', 'city', 'oldsite', 'resort', 'cliffside', 'finale']
+        },
+        finale: {
+            next: { name: 'finale', offset: 38.441 },
+            fades: ['intro', 'city', 'oldsite', 'resort', 'cliffside', 'finale']
+        }
+    },
+
+};
+
+const fade = 1;
+const base = 10;
+const curveLen = fade * ctx.sampleRate;
+const logCurve = new Float32Array(curveLen);
+for (let i = 0; i < curveLen; i++) {
+    logCurve[curveLen - 1 - i] = Math.log(1 + base*(i/curveLen)) / Math.log(1 + base);
+}
+const expCurve = new Float32Array(curveLen);
+for (let i = 0; i < curveLen; i++) {
+    expCurve[i] = 1 - logCurve[i];
+}
 
 function fetchAudio(url) {
     return new Promise((resolve, reject) => {
@@ -26,38 +98,14 @@ function decodeAudio(ctx, data) {
     });
 }
 
-async function load() {
+async function load(song) {
 
-    const ctx = new AudioContext();
+    const tracks = trackData[song];
 
-    const tracks = {
-        intro: {
-            next: 'main'
-        },
-        main: {
-            next: 'main',
-            cuts: [
-                {t: 15.237, dest: 'outro'},
-                {t: 30.473, dest: 'outro'},
-                {t: 45.706, dest: 'outro'},
-                {t: 60.951, dest: 'outro'},
-                // {t: 76.189, dest: 'outro'},
-                {t: 91.427, dest: 'outro'},
-                {t: 106.661, dest: 'outro'},
-                {t: 121.905, dest: 'outro'},
-                {t: 139.042, dest: 'outro'},
-                {t: 0, dest: 'outro'},
-            ]
-        },
-        outro: {
-            next: undefined
-        }
-    };
-
-    for (const [f, track] of Object.entries(tracks)) {
+    await Promise.all(Object.entries(tracks).map(async ([f, track]) => {
         track.name = f;
         if (!track.cuts) track.cuts = [];
-        track.buf = await decodeAudio(ctx, await fetchAudio(`snd/flowerdance/${f}.ogg`));
+        track.buf = await decodeAudio(ctx, await fetchAudio(`snd/${song}/${f}.ogg`));
         if (track.cuts.length) {
             track.cuts[track.cuts.length-1].t = track.buf.duration;
         }
@@ -74,7 +122,7 @@ async function load() {
             }
             track.waveform[i] = { min, max };
         }
-    }
+    }));
 
     const drawContainer = document.getElementById('draw');
     drawContainer.style.paddingBottom = ch+'px';
@@ -96,6 +144,7 @@ async function load() {
     let cutActive = -1;
 
     const xpos = t => Math.max(cw * t / track.buf.duration | 0, 0);
+
     const fullDraw = target => {
         const wf = target.waveform;
         baseCtx.clearRect(0, 0, cw, ch);
@@ -105,21 +154,25 @@ async function load() {
         }
         baseCtx.fillStyle = colors.cut;
         for (const cut of track.cuts) {
-            baseCtx.fillRect(xpos(cut.t) - 2, 0, 3, ch);
+            baseCtx.fillRect(xpos(cut.t) - Math.ceil(widths.cut/2), 0, widths.cut, ch);
         }
     };
 
-    const start = (target, t, offset = 0) => {
-        if (track.source) track.source.stop(t);
+    const start = (target, t, opts = {}) => {
+        if (track.source) track.source.stop(opts.stopTime || t);
         track = target;
         cutActive = -1;
-        startTime = t - offset;
+        startTime = t - (opts.offset || 0);
         target.source = ctx.createBufferSource();
         target.source.buffer = target.buf;
         target.source.connect(target.gain);
-        target.source.start(t, offset);
+        target.source.start(t, opts.offset || 0);
         fullDraw(target);
-        document.getElementById('status').textContent = 'current: ' + track.name + '; next: ' + track.next;
+        document.getElementById('status').textContent = 'current: ' + track.name + '; next: ' + track.next.name;
+    };
+
+    const startScript = (script, t) => {
+        start(tracks[script.name], t, script);
     };
 
     const msgEl = document.getElementById('msg');
@@ -129,126 +182,86 @@ async function load() {
 
     msg('ready');
 
+    let lastDraw = 0, lastGoto = -fade;
+
     document.addEventListener('keydown', e => {
         const t = ctx.currentTime;
-        switch (e.key) {
-            case 'q':
-                start(track, t + eps);
-                break;
-            case 'w':
+        if (e.key === 'q') {
+            start(track, t + eps);
+        } else if (e.key === 'w') {
+            if (cutActive === -1) {
+                cutActive = track.cuts.findIndex(cut => t < startTime + cut.t - lookahead);
                 if (cutActive === -1) {
-                    cutActive = track.cuts.findIndex(cut => t < startTime + cut.t - lookahead);
-                    if (cutActive === -1) {
-                        msg('no cut');
-                    } else {
-                        baseCtx.fillStyle = colors.cutActive;
-                        baseCtx.fillRect(xpos(track.cuts[cutActive].t) - 1, 0, 3, ch);
-                        msg('cut activated');
-                    }
+                    msg('no cut');
                 } else {
-                    baseCtx.fillStyle = colors.cut;
-                    baseCtx.fillRect(xpos(track.cuts[cutActive].t) - 1, 0, 3, ch);
-                    cutActive = -1;
-                    msg('cut deactivated');
+                    baseCtx.fillStyle = colors.cutActive;
+                    baseCtx.fillRect(xpos(track.cuts[cutActive].t) - Math.ceil(widths.cut/2), 0, widths.cut, ch);
+                    msg('cut activated');
                 }
-                break;
+            } else {
+                baseCtx.fillStyle = colors.cut;
+                baseCtx.fillRect(xpos(track.cuts[cutActive].t) - Math.ceil(widths.cut/2), 0, widths.cut, ch);
+                cutActive = -1;
+                msg('cut deactivated');
+            }
+        } else if (/[1-9]/.test(e.key)) {
+            const newTrack = track.fades[+e.key - 1];
+            if (newTrack) {
+                if (track.name === newTrack) {
+                    msg('no fade to self');
+                } else if (t - lastGoto < fade + eps) {
+                    msg('still fading');
+                } else {
+                    lastGoto = t + eps*2;
+                    track.gain.gain.setValueCurveAtTime(logCurve, lastGoto, fade);
+                    start(tracks[newTrack], lastGoto, {
+                        stopTime: lastGoto + fade,
+                        offset: lastGoto - startTime
+                    });
+                    track.gain.gain.setValueAtTime(0, t+eps);
+                    track.gain.gain.setValueCurveAtTime(expCurve, lastGoto, fade);
+                }
+            }
         }
     });
 
     barCnv.addEventListener('mousedown', e => {
         const t = ctx.currentTime + eps;
-        start(track, t, (e.offsetX / cw) * track.buf.duration);
+        start(track, t, {
+            offset: (e.offsetX / cw) * track.buf.duration
+        });
     });
 
-    let lastDraw = 0;
     const intr = setInterval(() => {
         const t = ctx.currentTime;
         const endTime = startTime + track.buf.duration;
 
-        barCtx.clearRect(lastDraw, 0, 1, ch);
+        barCtx.clearRect(lastDraw - Math.ceil(widths.bar/2), 0, widths.bar, ch);
         lastDraw = xpos(t - startTime);
-        barCtx.fillRect(lastDraw, 0, 1, ch);
+        barCtx.fillRect(lastDraw - Math.ceil(widths.bar/2), 0, widths.bar, ch);
 
         if (t >= endTime) {
             clearInterval(intr);
             msg('ended');
         } else if (cutActive >= 0 && startTime + track.cuts[cutActive].t - t < lookahead) {
-            start(tracks[track.cuts[cutActive].dest], startTime + track.cuts[cutActive].t);
+            startScript(track.cuts[cutActive].dest, startTime + track.cuts[cutActive].t);
         } else if (endTime - t < lookahead && track.next) {
-            start(tracks[track.next], endTime);
+            startScript(track.next, endTime);
         }
     }, tickRate);
 
 }
 
-async function loadCeleste() {
-    const ctx = new AudioContext();
-    const lvls = await Promise.all(['00intro', '01city', '02oldsite', '03resort', '04cliffside', '06finale']
-        .map(async f => {
-            const buf = await decodeAudio(ctx, await fetchAudio(`snd/mus_lvl7_summit_${f}.ogg`));
-            const source = ctx.createBufferSource();
-            const gain = ctx.createGain();
-            gain.gain.value = 0;
-            source.buffer = buf;
-            source.connect(gain).connect(ctx.destination);
-            return {
-                source, gain
-            };
-        }));
-    lvls[0].gain.gain.value = 1;
-    const fade = 1;
-
-    const base = 10;
-    const curveLen = fade * ctx.sampleRate;
-
-    let logCurve = new Float32Array(curveLen);
-    for (let i = 0; i < curveLen; i++) {
-        logCurve[curveLen - 1 - i] = Math.log(1 + base*(i/curveLen)) / Math.log(1 + base);
-    }
-
-    let expCurve = new Float32Array(curveLen);
-    for (let i = 0; i < curveLen; i++) {
-        expCurve[i] = 1 - logCurve[i];
-    }
-
-    const msgEl = document.getElementById('msg');
-    const msg = s => {
-        msgEl.textContent = `[${ctx.currentTime}] ${s}`;
-    };
-
-    msg('ready');
-
-    let i = 0, lastGoto = -fade;
-    const goto = j => {
-        const t = ctx.currentTime;
-        if (t - lastGoto < fade + eps) {
-            msg('still fading');
-        } else {
-            lastGoto = t;
-            lvls[i].gain.gain.setValueCurveAtTime(logCurve, t, fade);
-            lvls[i = j].gain.gain.setValueCurveAtTime(expCurve, t, fade);
-        }
-    };
-
-    document.addEventListener('keydown', function(e) {
-        switch (e.key) {
-            case 'q':
-                const t = ctx.currentTime + eps;
-                for (const lvl of lvls) {
-                    lvl.source.start(t, 40);
-                }
-                break;
-            case '1': goto(0); break;
-            case '2': goto(1); break;
-            case '3': goto(2); break;
-            case '4': goto(3); break;
-            case '5': goto(4); break;
-            case '6': goto(5); break;
-        }
-    });
-}
-
 
 window.addEventListener('load', () => {
-    loadFlower();
+    const btns = document.getElementById('btns');
+    for (const k of Object.keys(trackData)) {
+        const btn = document.createElement('button');
+        btn.textContent = k;
+        btn.addEventListener('click', () => {
+            load(k);
+            btns.style.display = 'none';
+        });
+        btns.appendChild(btn);
+    }
 });

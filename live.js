320671437d1b5f03840a184527b16a4820890832
Andy Tockman <andy@tck.mn>
2024-02-24 02:58:47 -0500

    move things to where they make more sense

--- live.js
+++ live.js
@@ -2,26 +2,19 @@ import { Player } from './player.js';
 import { fetchJSON } from './util.js';
 
 window.addEventListener('load', async () => {
-
     const player = new Player({
         btns: document.getElementById('btns'),
-        msg: document.getElementById('msg'),
-        metronome: document.getElementById('metronome'),
         draw: document.getElementById('draw'),
-        status: document.getElementById('status'),
+        metronome: document.getElementById('metronome'),
+        msg: document.getElementById('msg'),
         notes: document.getElementById('notes'),
+        status: document.getElementById('status'),
     });
 
     const songData = await fetchJSON('songdata.json');
     for (const k of Object.keys(songData)) songData[k].name = k;
 
     const tipTimerList = await fetchJSON('snd/tiptimer/list.json');
-
-    if (location.search) {
-        const opts = Object.fromEntries(new URLSearchParams('name=' + location.search.slice(1)).entries());
-        void player.load(songData[opts.name], opts);
-        return;
-    }
 
     const patter = document.getElementById('patter');
     const singer = document.getElementById('singer');
@@ -34,5 +27,10 @@ window.addEventListener('load', async () => {
             void player.load(songData[k]);
         });
         (songData[k].flavor === 'patter' ? patter : singer).appendChild(btn);
+    }
+
+    if (location.search) {
+        const opts = Object.fromEntries(new URLSearchParams('name=' + location.search.slice(1)).entries());
+        void player.load(songData[opts.name], opts);
     }
 });
